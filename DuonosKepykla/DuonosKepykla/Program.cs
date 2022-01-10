39b8eafd83b8a9c9d9caa9067c964871dc19cd2d
Arunas Varacinskas <arunas.varacinskas@gmail.com>
2022-01-10 21:28:13 +0200

    Duonos kepyka v1

---
+++ DuonosKepykla/DuonosKepykla/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DuonosKepykla
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kiek darbuotojas gali iskepti duonos per valanda?");
+            int kepaDuona = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Kiek kepykla turi darbuotoju?");
+            int kiekDarbuotoju = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Kokia duonos savikaina?");
+            double savikaina = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Duonos pardavimo kaina?");
+            double pardavimoKaina = Convert.ToDouble(Console.ReadLine());
+
+            int darboValandos = 8;
+
+            var duonaPerValanda = kiekDarbuotoju * 1;
+            var kepaPerValanda = kiekDarbuotoju * 1;
+            var pardavimai = pardavimoKaina + savikaina * kepaDuona;
+
+            Console.WriteLine($"gali iskepti: {duonaPerValanda} \nKepykla turi darbuotoju:{kiekDarbuotoju} \nDuonos savikaina:{savikaina}\nDuonos pardavimo kaina:{pardavimoKaina}");
+            Console.WriteLine($"Spes iskpeti:{duonaPerValanda}\nPelnas:{pardavimai}");
+            Console.ReadLine();
+
+
+
+
+
+
+        }
+    }
+}
